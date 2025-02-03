ea348596de82dc670ebeb6fedac95d8c10b0a00d
Bruno Silva <bruno.ogs@gmail.com>
2025-02-02 22:38:02 -0500

    adicionar funcionalidade de web scrapping do mercado livre

--- src/coleta/spiders/mercadolivre.py
+++ src/coleta/spiders/mercadolivre.py
@@ -5,6 +5,8 @@ class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     allowed_domains = ["lista.mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/tennis-corrida-masculino"]
+    page_count = 1
+    max_pages = 10
 
     def parse(self, response):
         products = response.css('div.poly-card__content')
@@ -23,7 +25,10 @@ class MercadolivreSpider(scrapy.Spider):
                 'new_price_cents' : cents[0] if len(cents) > 0 else None,
                 'reviews_rating_number' : product.css('span.poly-reviews__total::text').get(),  
                 'reviews_amount' : product.css('span.poly-reviews__rating::text').get()  
-
             }
-        
-        pass
+
+        if self.page_count < self.max_pages:
+            next_page = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
+            if next_page:
+                self.page_count += 1
+                yield scrapy.Request(url=next_page, callback=self.parse)
